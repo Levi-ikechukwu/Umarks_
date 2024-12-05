bd5bd9f4bf429929187227461e3a16a492e00a36
Levi Ikechukwu <leviikechukwu12@gmail.com>
2024-12-05 12:49:41 +0100

    mobile-Responsiveness

--- assets/script/script.js
+++ assets/script/script.js
@@ -20,19 +20,3 @@ close1.addEventListener("click", function() {
     modal.style.display = "none"
 })
 
-var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-});
