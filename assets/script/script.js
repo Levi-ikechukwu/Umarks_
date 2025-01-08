f1c3b766fa441eeba827963dbab706cbd49c96d8
Levi Ikechukwu <leviikechukwu12@gmail.com>
2025-01-08 21:55:08 +0100

    mobile-Responsiveness

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,22 +1,30 @@
 AOS.init();
 
-var overlay = document.getElementById("nav-overlay");
-var closeicon = document.getElementById("closeicon");
-var menu = document.getElementById("menu");
+// var overlay = document.getElementById("nav-overlay");
+// var closeicon = document.getElementById("closeicon");
+// var menu = document.getElementById("menu");
 
-menu.addEventListener("click", function() {
-    overlay.style.display = "flex"
+// menu.addEventListener("click", function() {
+//     overlay.style.display = "flex"
 
-})
-closeicon.addEventListener("click", function() {
-    overlay.style.display = "none"
-})
+// })
+// closeicon.addEventListener("click", function() {
+//     overlay.style.display = "none"
+// })
 
-logo.addEventListener("click", function() {
-    modal.style.display = "flex"
-})
+// logo.addEventListener("click", function() {
+//     modal.style.display = "flex"
+// })
 
-close1.addEventListener("click", function() {
-    modal.style.display = "none"
-})
+// close1.addEventListener("click", function() {
+//     modal.style.display = "none"
+// })
 
+function show(){
+    const sidebar = document.querySelector('.nav-mobile');
+    sidebar.style.display = 'flex';
+}
+function hide(){
+    const sidebar = document.querySelector('.nav-mobile');
+    sidebar.style.display = 'none';
+}
